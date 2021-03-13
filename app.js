bed16b588857e3a8977b0b676e7571d0d36a0985
taakuuyaa <51745522+taakuuyaa@users.noreply.github.com>
2021-03-13 22:15:56 +0900

    update app.js

--- app.js
+++ app.js
@@ -5,6 +5,12 @@ app.listen(8081, () => {
   console.log('Running at Port 8081...');
 });
 
+app.use((req, res, next) => {
+  console.log(req)
+  next()
+});
+
+// 数字文字列のみ
 app.get('/items/:itemId(\\d+)', (req, res) => {
   res.send(`request itemId: ${req.params.itemId}`)
 });
